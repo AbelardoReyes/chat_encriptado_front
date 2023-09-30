6c0d662fc0a30e234c4edd7c06d13dad787b39b1
Abelardo Garcia Reyes <abelardoreyes256@gmail.com>
2023-09-30 17:45:54 -0600

    [ADD ya encriptan y desencriptan los mensajes

--- src/app/components/sections/chat/chat-socket/chat-socket.component.ts
+++ src/app/components/sections/chat/chat-socket/chat-socket.component.ts
@@ -13,6 +13,7 @@ import * as CryptoJS from 'crypto-js';
   styleUrls: ['./chat-socket.component.css']
 })
 export class ChatSocketComponent implements OnInit, AfterViewChecked {
+  secretKey = 'clave'; // Reemplaza con tu clave compartida
   @ViewChild('scrollToBottom') scrollToBottom: ElementRef | undefined;
   nickname: any;
   title = 'chat_front';
@@ -34,15 +35,19 @@ export class ChatSocketComponent implements OnInit, AfterViewChecked {
       this.socketIdPropio = this.socket.id;
       //console.log("Id propio", this.socketIdPropio);
     });
+
     this.socket.emit('getAllMessages', true);
     this.socket.on('allMessages', (data: any) => {
-      console.log('Mensajes recibidos del servidor: ', data);
+      //console.log('Mensajes recibidos del servidor: ', data);
       this.messages = [...data];
     });
 
     this.socket.on('message', (data: any) => {
-      console.log('Mensaje recibido del servidor: 2', data);
-      this.messages.push(data);
+      //console.log('Mensaje recibido del servidor: 2', data);
+      // Desencriptar el mensaje antes de mostrarlo
+      const mensajeDesencriptado = this.decryptMessage(data);
+      this.messages.push(mensajeDesencriptado);
+
       console.log('Arreglo messages actualizado:', this.messages);
       setTimeout(() => {
         this.cd.detectChanges();
@@ -59,12 +64,12 @@ export class ChatSocketComponent implements OnInit, AfterViewChecked {
   }
 
   sendMessage() {
-    //console.log("User", this.user);
-    //console.log(this.form.value);
     this.form.value.socket_id = this.user.username;
     this.form.value.user_id = this.user.id;
     //console.log("Los datos que se enviaran son: ", this.form.value);
+    this.form.value.message = this.encryptMessage(this.form.value.message);
     this.socket.emit('sendMessage', this.form.value);
+    this.form.reset();
   }
 
   getUser() {
@@ -75,18 +80,28 @@ export class ChatSocketComponent implements OnInit, AfterViewChecked {
       }
     );
   }
-  handleInput(event: any) {
-    const inputValue = event.target.value;
-    if (inputValue.length === 30) {
-      event.target.value += '\n';
-      this.form.get('message')?.setValue(event.target.value);
-    }
+  // Función para cifrar un mensaje
+  encryptMessage(message: string): string {
+    const ciphertext = CryptoJS.AES.encrypt(message, this.secretKey).toString();
+    return ciphertext;
   }
+
+  // Función para descifrar un mensaje
+  decryptMessage(ciphertext: any): string {
+    const crypto =  ciphertext.message;
+    console.log('Mensaje cifrado: ', crypto);
+    const bytes = CryptoJS.AES.decrypt(crypto, this.secretKey);
+    const originalText = bytes.toString(CryptoJS.enc.Utf8);
+    ciphertext.message = originalText;
+    return ciphertext;
+  }
+
   ngAfterViewChecked() {
-    console.log('ngAfterViewChecked called');
     if (this.scrollToBottom && this.scrollToBottom.nativeElement) {
       this.scrollToBottom.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
     }
   }
+
+
 }
 
